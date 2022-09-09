e04ce15cd946e49d6d11b196659c0d457114102a
Valenti <Valentin.Zhdanovskiy@gmail.com>
2022-09-09 12:40:42 +0300

    5 commit

--- js/main-burger.js
+++ js/main-burger.js
@@ -1,13 +1,13 @@
 // const menuNav = document.querySelector('.menu-nav');
 const menu = document.querySelector('.menu');
-const mobilMenu = document.querySelector('.mobil-menu');
+const menuHamb = document.querySelector('.menu-hamb');
 // const burgerMenu = document.querySelector('.burger-menu');
+console.log('menu');
 
 
 
-
-mobilMenu.onclick = function() {
-  mobilMenu.classList.toggle('open');
+menuHamb.onclick = function() {
+  // mobilMenu.classList.toggle('open');
   menu.classList.toggle('open');
 };
 
